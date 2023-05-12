ffb87baec1989f846284b18cbea9d95dbd9e9992
Arin jain <arinjain349@gmail.com>
2023-05-12 11:12:19 +0530

    Added the splash screen and created the next page named Time_Table

--- app/src/main/java/com/example/base/MainActivity.java
+++ app/src/main/java/com/example/base/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.base;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //variable for duration
+    private static int SPLASH_SCREEN = 4000;
+
+
+    //SPLASH SCREEN
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        //setContentView(R.layout.activity_main);
+
+
+
+        //SPLASH SCREEN
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        //Hooks
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView3);
+        //Calling
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        //Intend Call
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Time_Table.class);
+                startActivity(intent );
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+
     }
 }
--- app/src/main/java/com/example/base/SplashActivity.java
+++ app/src/main/java/com/example/base/SplashActivity.java
@@ -3,12 +3,39 @@ package com.example.base;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    //Variables
+        Animation topAnim, bottomAnim;
+        ImageView image;
+        TextView logo, slogan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView3);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+ 
+
     }
 }
