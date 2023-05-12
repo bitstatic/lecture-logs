b7894df8553244b4e1b1fd15fa0c538cb0022b24
Arin jain <arinjain349@gmail.com>
2023-05-12 11:11:39 +0530

    Added the splash screen and created the next page named Time_Table

---
+++ app/src/main/java/com/example/base/Time_Table.java
@@ -0,0 +1,14 @@
+package com.example.base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Time_Table extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_table);
+    }
+}
